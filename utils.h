abcc2d361a364d9bee7fceaba77fbe1bc06b5ad9
Otavie <otavieokuoyo.gmail.com>
2023-07-26 02:03:30 -0700

    utils.h follow betty stndd

--- utils.h
+++ utils.h
@@ -1,10 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
-
 extern char **environ;
 char *read_command();
 char **parse_command(char *command);
 void execute_command(char **args);
 void handle_signal(int signal);
-
 #endif
