b0764c49f9f9045b9419dee6d75ae3564ec02421
mathewCodex <mathewemmanuel212@gmail.com>
2023-07-27 16:07:42 -0700

    modified shell

--- utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-#define MAX_COMMAND_LENGTH 1024
-void handle_signal(int signal);
-char *read_command(void);
-char **parse_command(char *command);
-void execute_command(char **args);
-#endif
